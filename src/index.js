e92ec7b91d0ef5adc8d3e2c4590ed3beb307e0b5
Wiraa <ur.wiraananda@gmail.com>
2024-10-12 14:47:51 +0800

    feat: menambahkan fitur harus mengisi username dsb

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-const express = require("express"); // Mengimpor framework Express
-const { PrismaClient } = require("@prisma/client"); // Mengimpor Prisma Client untuk berinteraksi dengan database
-const bodyParser = require("body-parser"); // Middleware untuk parsing body JSON
+const express = require("express");
+const bodyParser = require("body-parser");
 const router = require("./user/user.controller");
 
-const app = express(); // Membuat instance Express
+const app = express();
 
-// Menjalankan server
-const PORT = process.env.PORT || 2000; // Mendapatkan PORT dari environment variable atau default ke 2000
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`); // Mencetak pesan bahwa server sedang berjalan
+  console.log(`Server running on port ${PORT}`);
 });
 
-// Middleware untuk parse JSON
-app.use(bodyParser.json()); // Menggunakan body-parser untuk menangani permintaan JSON
+app.use(bodyParser.json());
 
 app.use("/user", router);
